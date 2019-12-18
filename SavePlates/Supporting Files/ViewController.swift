f35e351af25f584db2039d3fda2736bdc98ac75c
Sam Roman <samroman@Sams-MacBook-Pro.local>
2019-12-18 12:01:21 -0500

    Initial commit with folder heirarchy

---
+++ SavePlates/Supporting Files/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SavePlates
+//
+//  Created by Sam Roman on 12/18/19.
+//  Copyright © 2019 Sam Roman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
